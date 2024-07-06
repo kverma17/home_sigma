d9d6867339ce3d5c99bbcfe6fb48e77a89deb16a
Karan Verma <rockinkaran@yahoo.com>
2024-07-07 01:20:53 +0530

    optimise images

--- core/models.py
+++ core/models.py
@@ -1,6 +1,11 @@
+import os
 from django.db import models 
 from django import forms
 from django.core.exceptions import ValidationError
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+
 # Create your models here. 
 
 
@@ -29,7 +34,7 @@ class Property(models.Model):
     name = models.CharField(max_length=50)
     url = models.CharField(max_length=50)
     detail = models.CharField(max_length=100, blank=True)
-    description = models.CharField(max_length=500, blank=True)
+    description = models.TextField(blank=True)
     category = models.CharField(max_length=50, blank=True)
     label = models.CharField(max_length=255, blank=True)
     builder = models.CharField(max_length=30, blank=True)
@@ -45,7 +50,7 @@ class Property(models.Model):
     thumbnail = models.ImageField(upload_to='property_images/')
     community = models.CharField(max_length=50, blank=True)
     community_image_url = models.ImageField(upload_to='property_images/', blank=True)
-    community_description = models.CharField(max_length=500, blank=True)
+    community_description = models.TextField(blank=True)
     street = models.CharField(max_length=30, blank=True)
     city = models.CharField(max_length=30, blank=True)
     state = models.CharField(max_length=30, blank=True)
@@ -110,3 +115,20 @@ class PropertyImage(models.Model):
     property_id = models.IntegerField()
     image_url = models.ImageField(upload_to='property_images/')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Open the uploaded image
+        img = Image.open(self.image_url)
+
+        # Resize and compress the image
+        output_size = (800, 800)  # You can adjust this size as needed
+        img.thumbnail(output_size, Image.LANCZOS)
+        buffer = BytesIO()
+        img.save(buffer, format='JPEG', quality=85)
+        buffer.seek(0)
+
+        # Save the new image to the same field
+        new_filename = os.path.splitext(self.image_url.name)[0] + '.jpg'
+        self.image_url.save(new_filename, ContentFile(buffer.read()), save=False)
+
+        super().save(*args, **kwargs)
